67f7f467f72e01f000eb6498b8f1f7fa0933a204
XinyiChen <cxinyic@sjtu.edu.cn>
2020-07-06 21:29:31 -0400

    add debug info

--- managers/processor/pcache/fault.c
+++ managers/processor/pcache/fault.c
@@ -164,6 +164,7 @@ int common_do_fill_page(struct mm_struct *mm, unsigned long address,
 	ret = pcache_add_rmap(pcm, page_table, address,
 			      mm, current->group_leader, caller);
 	if (unlikely(ret)) {
+		pr_info("bad return11: %lu\n", ret);
 		pte_clear(page_table);
 		ret = VM_FAULT_OOM;
 		goto out;
@@ -337,6 +338,7 @@ __pcache_do_zerofill_page(unsigned long address, unsigned long flags,
 
 	pcache_kva = pcache_meta_to_kva(pcm);
 	memset(pcache_kva, 0, PCACHE_LINE_SIZE);
+	pr_info("Debug:pcache_do_zerofill_page");
 
 	/*
 	 * Notify remote memory about this zerofill.

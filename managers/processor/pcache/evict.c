547156788019847cbedf82d9005731e53b801678
XinyiChen <cxinyic@sjtu.edu.cn>
2021-01-06 14:44:58 -0500

    debug in eviction

--- managers/processor/pcache/evict.c
+++ managers/processor/pcache/evict.c
@@ -247,7 +247,7 @@ int pcache_evict_line(struct pcache_set *pset, unsigned long address,
 	inc_pset_event(pset, PSET_EVICTION);
 	inc_pcache_event(PCACHE_EVICTION_SUCCEED);
 	nr_evict_lines +=1;
-	if(nr_evict_lines%1000==0){
+	if(nr_evict_lines%10000==0){
 		printk("DepTrack: evict %d lines\n", nr_evict_lines);
 	}
 	return PCACHE_EVICT_SUCCEED;

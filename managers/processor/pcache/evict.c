38df24a950047645a443b8a2c1ee74e0eba82acf
XinyiChen <cxinyic@sjtu.edu.cn>
2021-01-06 18:58:16 -0500

    debug

--- managers/processor/pcache/evict.c
+++ managers/processor/pcache/evict.c
@@ -181,14 +181,14 @@ int pcache_evict_line(struct pcache_set *pset, unsigned long address,
 	PCACHE_BUG_ON_PCM(!PcacheReclaim(pcm), pcm);
 	if (nr_dp_info!=0){
 		spin_lock(&dp_spinlock);
-		for (index = 0; index < dp_vector_size(dp_info_list); index++){
+		/*for (index = 0; index < dp_vector_size(dp_info_list); index++){
 			curr_dp_info = (struct dp_info*)dp_vector_Nth(dp_info_list, index);
             for(i=0;i<curr_dp_info->nr_pages;i++){
 				if(*(curr_dp_info->pcm_list+i) == pcm){
 					nr_evict_mmaped_lines +=1;
 				}
 			}
-		}
+		}*/
 		spin_unlock(&dp_spinlock);
 
 	}

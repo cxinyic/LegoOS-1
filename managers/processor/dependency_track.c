4718be1062ea27c9f91d01bdf1516ce088625464
XinyiChen <cxinyic@sjtu.edu.cn>
2021-01-10 18:22:06 -0500

    debug

--- managers/processor/dependency_track.c
+++ managers/processor/dependency_track.c
@@ -147,6 +147,7 @@ static int dependency_track(void *unused){
                      pcm->dependency_list = (struct dp_vector*)kmalloc(sizeof(struct dp_vector), GFP_KERNEL);
                      dp_vector_new(pcm->dependency_list, sizeof(struct pcache_meta*));
                  }
+                 init_flag = 1;
             }
 
             struct rmap_walk_control rwc = {

1eedc96e2b6c9cf556a3986529ac1675f9ab188a
XinyiChen <cxinyic@sjtu.edu.cn>
2021-01-15 21:47:59 -0500

    debug

--- managers/processor/pcache/evict.c
+++ managers/processor/pcache/evict.c
@@ -153,6 +153,7 @@ static int __flush_if_dirty(struct pcache_meta *pcm, struct pcache_rmap *rmap, v
 		if (!pte_none(*pte) && pte_present(*pte)) {
 			if (likely(pte_dirty(*pte))) {
 				*pte = pte_mkclean(*pte);
+				pcm->prev_dirty = 1;
 				// pcache_flush_one(pcm);
 				fdi->nr_dirty_pages += 1;
 			}

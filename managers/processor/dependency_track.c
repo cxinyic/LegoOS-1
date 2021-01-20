a7297ae1a55a7663f20864db33a077336674dd19
XinyiChen <cxinyic@sjtu.edu.cn>
2021-01-20 14:21:36 -0500

    debug

--- managers/processor/dependency_track.c
+++ managers/processor/dependency_track.c
@@ -406,21 +406,22 @@ static int dependency_track(void *unused){
 
                
                // printk("DepTrack: called flush_register_value successfully\n");
-               flush_flag +=1;
+               
            }
            if (pdi.nr_dirty_pages>0 && pdi.nr_dirty_pages< 100 && flush_flag == 1){
                printk("DepTrack: in this periods, the number of dirty pages are %d\n", pdi.nr_dirty_pages);
                printk("DepTrack: the ip value is %lx\n", current_registers->ip);
                flush_register_value(NULL);
                printk("DepTrack: called flush_register_value successfully\n");
-               flush_flag +=1;
+               
            }
            if (pdi.nr_dirty_pages>0 && pdi.nr_dirty_pages< 100 && flush_flag == 2){
                printk("DepTrack: in this periods, the number of dirty pages are %d\n", pdi.nr_dirty_pages);
                get_register_value(NULL);
                printk("DepTrack: called get_register_value successfully\n");
-               flush_flag +=1;
+               
            }
+           flush_flag +=1;
            
            
        

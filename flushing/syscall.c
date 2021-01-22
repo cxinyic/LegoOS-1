64e841e5ae51253bafe0c7413d16cf5453fa5193
XinyiChen <cxinyic@sjtu.edu.cn>
2021-01-22 14:35:12 -0500

    debug

--- flushing/syscall.c
+++ flushing/syscall.c
@@ -14,17 +14,21 @@ asmlinkage long sys_mmap_track(unsigned long addr, unsigned long len,
         unsigned long prot, unsigned long flags, 
         unsigned long fd, unsigned long off)
 {
+	
+#ifdef CONFIG_COMP_PROCESSOR
 	current_pid = current->pid;
 	current_tgid = current->tgid;
 	current_tsk = current;
+#endif
 	return 0;
-	
+
 }
 asmlinkage long sys_munmap_track(unsigned long addr, size_t len)
 {
-	
+#ifdef CONFIG_COMP_PROCESSOR
 	current_pid = -1;
 	current_tgid = -1;
 	current_tsk = NULL;
+#endif
 	return 0;
 }

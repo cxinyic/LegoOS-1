d7c6d7ca6995b8f886450dcb3960d66048636ea6
XinyiChen <cxinyic@sjtu.edu.cn>
2021-01-22 14:30:11 -0500

    debug

--- include/processor/processor.h
+++ include/processor/processor.h
@@ -37,9 +37,10 @@ void pcache_thread_exit(struct task_struct *tsk);
 #ifdef CONFIG_CHECKPOINT
 int checkpoint_thread(struct task_struct *);
 #else
-int deptrack_checkpoint_thread(struct task_struct *);
+
 static inline int checkpoint_thread(struct task_struct *tsk) { return 0; }
 #endif
+int deptrack_checkpoint_thread(struct task_struct *);
 
 int do_execve(const char *filename,
 	      const char * const *argv,

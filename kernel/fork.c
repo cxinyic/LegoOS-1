bd60405cd5c3a571b846fd0e0bff9224c170c9d4
XinyiChen <cxinyic@sjtu.edu.cn>
2021-01-27 19:09:43 -0500

    debug

--- kernel/fork.c
+++ kernel/fork.c
@@ -997,7 +997,7 @@ pid_t do_fork(unsigned long clone_flags,
 		get_task_struct(p);
 	}
 	
-	printk("Pid is %d\n", p->pid);
+	/*printk("Pid is %d\n", p->pid);
 	printk("sp is %lx\n", task_pt_regs(p)->sp);
 	printk("ip is %lx\n", task_pt_regs(p)->ip);
 	printk("cs is %lx\n", task_pt_regs(p)->cs);
@@ -1021,7 +1021,7 @@ pid_t do_fork(unsigned long clone_flags,
 
     printk("sp is %lx\n", task_pt_regs(p)->sp);
 	printk("ip is %lx\n", task_pt_regs(p)->ip);
-	printk("cs is %lx\n", task_pt_regs(p)->cs);
+	printk("cs is %lx\n", task_pt_regs(p)->cs);*/
 	wake_up_new_task(p);
 
 	if (clone_flags & CLONE_VFORK)

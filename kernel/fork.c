abbc6e8bc284dee42303d40bde7deb0c9e79610e
XinyiChen <cxinyic@sjtu.edu.cn>
2021-01-31 12:42:30 -0500

    debug

--- kernel/fork.c
+++ kernel/fork.c
@@ -1000,7 +1000,8 @@ pid_t do_fork(unsigned long clone_flags,
 	printk("Pid is %d\n", p->pid);
 	printk("sp is %lx\n", task_pt_regs(p)->sp);
 	printk("ip is %lx\n", task_pt_regs(p)->ip);
-	printk("cs is %lx\n", task_pt_regs(p)->cs)/*
+	printk("cs is %lx\n", task_pt_regs(p)->cs);
+	/*
 	if (p->pid == 25){
 		deptrack_restore_files(current_info.pss);
 		deptrack_restore_signals(current_info.pss);

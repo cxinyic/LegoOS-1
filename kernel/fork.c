ce742579d5f9ffd3261371c3d1b963d322ce359e
XinyiChen <cxinyic@sjtu.edu.cn>
2021-02-03 14:40:24 -0500

    attempt: remove fs copy

--- kernel/fork.c
+++ kernel/fork.c
@@ -766,13 +766,13 @@ struct task_struct *copy_process(unsigned long clone_flags,
 	}
 	
 	p->pid = pid;
-	if(pid == 25){
+	/*if(pid == 25){
 		printk("pid is 25\n");
 		p->fs = current_tsk->fs;
 	}
-	else{
+	else{*/
 		printk("pid is not 25\n");
-	}
+	// }
 	if(pid == 25){
 		clone_flags |= CLONE_GLOBAL_THREAD;
 	}
@@ -784,7 +784,7 @@ struct task_struct *copy_process(unsigned long clone_flags,
 	/*
 	 * Now do the dirty work.
 	 */
-	if (pid == 25){
+	/*if (pid == 25){
 		strcpy(p->fs.cwd, current_tsk->fs.cwd);
 		strcpy(p->fs.root, current_tsk->fs.root);
 		memcpy((void*)(&p->fs.lock), (void*)(&current_tsk->fs.lock), sizeof(p->fs.lock));
@@ -793,9 +793,9 @@ struct task_struct *copy_process(unsigned long clone_flags,
 		retval = 0;
 
 	}
-	else{
+	else{*/
 		retval = copy_fs(p);
-	}
+	// }
 	if (retval < 0)
 		goto out_free;
 
